d3ce41f8e5d994eef25d813a13b60d741ba6ea57
jeet.gupta <jeetgupta@gmail.com>
2024-04-20 20:17:36 -0400

    creating routing features

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import "./App.css"
 import { Home } from'./pages/Home';
 import { AddClient } from './pages/AddClient';
-import { ClientProfile } from './pages/ClientProfile';
-import {Routes, Route} from "react-router-dom"
+import { ClientProfile } from './pages/ClientProfile';  
+import { FileUpload } from './pages/FileUpload';
+import {Routes, Route} from "react-router-dom";
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/add-client" element={<AddClient/>}/>
                 <Route path="/client-profile" element={<ClientProfile/>}/>
+                <Route path="/import-csv" element={<FileUpload/>}></Route>
             </Routes>
 
 
