a73970bee5a9d0c6fd98c5b7a39bf928f3952a41
jeet.gupta <jeetgupta@gmail.com>
2024-04-20 20:17:50 -0400

    added separate pages

---
+++ client/src/pages/FileUpload.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const FileUpload = () => {
+  return (
+    <div>
+        <h1>Database Import</h1>
+        <input type='file' accept='.csv'/>
+    </div>
+  )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Home.css'
 import { SearchBar } from '../components/SearchBar';
+import { Link } from 'react-router-dom'
 
 
 export const Home = () => {
     return (
-        <>
+        <div style={{textAlign: 'center'}}>
             <h1>Client Database</h1>
+            <Link style={{position: 'absolute', top: '20px', right: '30px'}} to='/import-csv'>[Import]</Link>
             <div className="search-bar-container"><SearchBar/></div>
-        </>
+        </div>
     )
 }
